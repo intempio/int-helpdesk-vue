d4aaa31ffca2db6f0da729a685f8f9c11ebcc440
Rustan Corpuz <rustanacexd@gmail.com>
2018-06-07 22:30:11 +0800

    add ac link on _id

--- pages/attendees/_id.vue
+++ pages/attendees/_id.vue
@@ -4,7 +4,33 @@
       <div class="columns is-mobile">
         <div class="column">
           <h4 class="title">{{full_name}} - {{email}}</h4>
-          <b-table :data="event_attendee" :columns="columns"></b-table>
+          <b-table :data="event_attendee">
+            <template scope="props">
+              <b-table-column field="event_name" label="Event Name">
+                {{ props.row.event.event_name }}
+              </b-table-column>
+
+              <b-table-column field="date" label="Event Date">
+                {{ props.row.event.date}}
+              </b-table-column>
+
+              <b-table-column field="redirect_lookup_id" label="Ac Link">
+                <a :href="'http://i17r.com/' + props.row.redirect_lookup_id"
+                   v-if="props.row.redirect_lookup_id">
+                  http://i17r.com/{{ props.row.redirect_lookup_id }}
+                </a>
+              </b-table-column>
+
+              <b-table-column field="pre_registered" label="Pre Registered">
+                {{ props.row.pre_registered}}
+              </b-table-column>
+
+
+              <b-table-column field="call_complete" label="Call Complete">
+                {{ props.row.call_complete}}
+              </b-table-column>
+            </template>
+          </b-table>
         </div>
       </div>
     </section>
@@ -23,28 +49,6 @@
         email,
         full_name,
         event_attendee
-      }
-    },
-    data() {
-      return {
-        columns: [
-          {
-            field: 'event.event_name',
-            label: 'Event Name',
-          },
-          {
-            field: 'event.date',
-            label: 'Event Date',
-          },
-          {
-            field: 'pre_registered',
-            label: 'Pre Registered',
-          },
-          {
-            field: 'call_complete',
-            label: 'Call Complete'
-          }
-        ]
       }
     }
   }
