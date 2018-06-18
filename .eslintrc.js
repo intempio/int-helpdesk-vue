ed947184a539699ff7cf58160661084890fd2ce9
Rustan Corpuz <rustanacexd@gmail.com>
2018-06-18 23:08:31 +0800

    update prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,10 +11,10 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential',
-    'prettier'
+    'prettier',
   ],
   // required to lint *.vue files
   plugins: ['vue'],
   // add your custom rules here
   rules: {},
-}
+};
