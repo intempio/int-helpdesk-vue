9af9a07d91c853dbb9120f51705c9000b79c4c66
marrajoy <marra@go2impact.com>
2018-06-22 21:28:51 +0800

    Added Home link

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,26 @@
     <div class="columns is-mobile">
       <div class="column">
         <b-field>
-          <img src="http://intempio.com/wp-content/uploads/2018/05/INTEMPIO_FINAL_Tag1.png" class="site-logo" />
+          <a href="/"><img src="http://intempio.com/wp-content/uploads/2018/05/INTEMPIO_FINAL_Tag1.png" class="site-logo" /></a>
         </b-field>
       </div>
       <div class="column is-half is-offset-one-quarter">
-        <div>
-          <b-field>
-            <b-input placeholder="Search..." type="search" size="is-large" icon="magnify" id="search" v-model="searchString">
-            </b-input>
-          </b-field>
+        <div class="columns">
+          <div class="column is-four-fifths is-offset-one-fifth s-field">
+            <div>
+              <b-field>
+                <b-input placeholder="Search..." type="search" size="is-large" icon="magnify" id="search" v-model="searchString">
+                </b-input>
+              </b-field>
+            </div>
+          </div>
+          <div class="column">
+            <b-field>
+              <a href="/">
+                <b-icon icon="home" size="is-medium"></b-icon>
+              </a>
+            </b-field>
+          </div>
         </div>
       </div>
     </div>
@@ -117,7 +128,7 @@ export default {
   },
   computed: {},
   methods: {
-    handleDone(event){
+    handleDone(event) {
       // this.$store.dispatch('UPDATE_DONE_EVENT_ATTENDEE', {
       //   eventAttendeeId: this.selected.eventAttendeeId,
       //   done: value
